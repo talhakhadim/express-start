5b5be023a4b2637a14b3d6df4097196457bc710a
Talha Khadim <67506264+talhakhadim@users.noreply.github.com>
2023-03-06 00:17:38 +0500

    rate limit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express=require('express');
+const rateLimit = require("express-rate-limit");
 const cors=require('cors');
 
 
@@ -11,6 +12,12 @@ app.use(cors({
     // origin: process.env.CLIENT_URL
     origin:"*"
 }));
+//rate limiter
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
 
 
 
