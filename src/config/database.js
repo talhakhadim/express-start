ac8c11fb44e348724d8bba94dffba45329355488
Talha Khadim <67506264+talhakhadim@users.noreply.github.com>
2023-04-08 23:41:25 +0500

    s

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const connectDB=async()=>{
     try{
         mongoose.set('debug',process.env.MONGO_DEBUG);
         mongoose.set('allowDiskUse',true);
-        const conn=await mongoose.connect(process.env.DB_URL,{
+        await mongoose.connect(process.env.DB_URL,{
             useNewUrlParser:true,
             useUnifiedTopology:true
         })
